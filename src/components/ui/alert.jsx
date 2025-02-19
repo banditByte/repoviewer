3dc48c36f56d30e18dcdc18c70e49d3454c84aca
banditByte <143024738+banditByte@users.noreply.github.com>
2025-02-19 17:43:22 +0200

    Update alert.jsx

--- src/components/ui/alert.jsx
+++ src/components/ui/alert.jsx
@@ -1,31 +1,25 @@
 import * as React from "react"
-import { cva } from "class-variance-authority"
 import { cn } from "@/lib/utils"
 
-const alertVariants = cva(
-  "relative w-full rounded-lg border p-4 [&>svg~*]:pl-7 [&>svg+div]:translate-y-[-3px] [&>svg]:absolute [&>svg]:left-4 [&>svg]:top-4 [&>svg]:text-foreground",
-  {
-    variants: {
-      variant: {
-        default: "bg-background text-foreground",
-        destructive:
-          "border-destructive/50 text-destructive dark:border-destructive [&>svg]:text-destructive",
-      },
-    },
-    defaultVariants: {
-      variant: "default",
-    },
+const Alert = React.forwardRef(({ className, variant = "default", ...props }, ref) => {
+  const variantStyles = {
+    default: "bg-white border-gray-200",
+    destructive: "bg-red-50 border-red-200 text-red-700",
   }
-)
 
-const Alert = React.forwardRef(({ className, variant, ...props }, ref) => (
-  <div
-    ref={ref}
-    role="alert"
-    className={cn(alertVariants({ variant }), className)}
-    {...props}
-  />
-))
+  return (
+    <div
+      ref={ref}
+      role="alert"
+      className={cn(
+        "relative w-full rounded-lg border p-4",
+        variantStyles[variant],
+        className
+      )}
+      {...props}
+    />
+  )
+})
 Alert.displayName = "Alert"
 
 const AlertTitle = React.forwardRef(({ className, ...props }, ref) => (
